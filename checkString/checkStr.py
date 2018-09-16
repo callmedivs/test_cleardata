6609ca8537102693ce1a1616974058d7991ff41c
callmedivs <divya@parksidelending.com>
2018-09-16 16:52:46 -0700

    Update checkStr.py

--- checkString/checkStr.py
+++ checkString/checkStr.py
@@ -9,9 +9,12 @@ class CheckStr:
         self.match_string = 'abcdefghijklmnopqrstuvwxyz'
     
     def check_alpha(self, input_string):
-        if len(input_string) < 26:
+        if str(input_string) == 'None':
+            return False
+        elif len(input_string) < 26:
             return False
         else:
             lowercase_input_list = list(str(input_string).lower())
             # run loop 26 times to make sure all the alphabets are present in the input string
             return all(each_alpha in lowercase_input_list for each_alpha in self.match_string)
+
